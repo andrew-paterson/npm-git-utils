bb4deee3dca686a21249ee49c462d0fd29106f88
Andrew Paterson <andrew.paterson9@gmail.com>
2023-07-09 14:09:20 +0200

    Abstract functions

---
+++ dependency-checks.js
@@ -0,0 +1,150 @@
+const getGitInfo = require('git-repo-info');
+var gitState = require('git-state');
+const fs = require('fs');
+const path = require('path');
+const chalk = require('chalk');
+
+module.exports = function (environment, ENV, deps, localDevRepos) {
+  if (environment === 'development' || environment === 'test') {
+    ENV.dependencySummary = {};
+    deps.forEach((dep) => {
+      checkDep(dep, ENV);
+    });
+    console.log(`----------------------------------\n${chalk.magenta(`DEPENDENCY VERSIONS`)}\n`);
+    console.log(ENV.dependencySummary);
+    if (!ENV.symlinks) {
+      console.log(`----------------------------------`);
+    }
+
+    if (ENV.symlinks) {
+      console.log(`----------------------------------\n${chalk.magenta(`SYMLINKS DETECTED`)}\n`);
+      console.log(ENV.symlinks);
+      console.log(`----------------------------------`);
+    }
+    checkLocalDevRepos(localDevRepos);
+  }
+};
+
+function checkLocalDevRepos(localDevRepos) {
+  if (!localDevRepos) {
+    return;
+  }
+  console.log(chalk.magenta(`LOCAL DEV REPO STATUS\n`));
+
+  const output = {};
+  localDevRepos.forEach((repo) => {
+    const depName = path.basename(repo.path);
+
+    output[depName] = {};
+    let repoState;
+    let parentName;
+
+    if (gitState.isGitSync(repo.path)) {
+      repoState = gitState.checkSync(repo.path);
+      const gitInfo = getGitInfo(repo.path);
+      let gitStatus = {};
+      for (var key in repoState) {
+        if (repoState[key] > 0) {
+          gitStatus[key] = repoState[key];
+        }
+      }
+      output[depName] = gitStatus || {};
+      output[depName].lastCommit = `${gitInfo.abbreviatedSha} "${gitInfo.commitMessage}"`;
+    }
+    if (repo.dependencyOf) {
+      parentName = path.basename(repo.dependencyOf);
+      const pathToDep = path.join(repo.dependencyOf, 'node_modules', depName);
+      const stats = fs.lstatSync(pathToDep) || {};
+      if (stats.isSymbolicLink()) {
+        output[depName].symlinked = true;
+      }
+    }
+
+    const depWarnings = [];
+    if (output[depName].symlinked) {
+      depWarnings.push(`is symlinked in ${parentName}`);
+    }
+    if (depWarnings.length) {
+      console.log(chalk.red(`WARNING: ${depName} ${depWarnings.join(' and ')}.`));
+    }
+  });
+  console.log(output);
+}
+
+function isSymlink(path) {
+  if (!path) {
+    return;
+  }
+  if (!fs.existsSync(path)) {
+    return;
+  }
+  const stats = fs.lstatSync(path);
+  if (!stats) {
+    return;
+  }
+  if (stats.isSymbolicLink()) {
+    return true;
+  }
+}
+
+function checkGitState(path) {
+  if (!gitState.isGitSync(path)) {
+    return '';
+  }
+  const repoState = gitState.checkSync(path);
+  return `${repoState.dirty} dirty and ${repoState.untracked} untracked.`;
+}
+
+function installedMatchesRequired(packageFilePath, depName) {
+  try {
+    const parentPackageName = (packageFilePath.match(/.*?node_modules\/(.*?)\/package\.json/) || [])[1];
+    const packageFile = require(packageFilePath);
+    const packageLockFilePath = `${process.cwd()}/package-lock.json`;
+    const packageLockFile = require(packageLockFilePath);
+    const packageDep = packageFile.devDependencies[depName] || packageFile.dependencies[depName];
+    if (!packageDep) {
+      return 'Not installed';
+    }
+    const packageLockListing = packageLockFile.dependencies[depName] || ((packageLockFile.dependencies[parentPackageName] || {}).dependencies || {})[depName];
+    if (packageLockListing) {
+      if (packageLockListing.from.split('#')[1] === packageDep.split('#')[1]) {
+        return packageLockListing.from.split('#')[1];
+      }
+      return {
+        installed: `${packageLockListing.from.split('#')[1]} <= ${packageLockFilePath.replace(process.cwd(), '')}`,
+        _required: `${packageDep.split('#')[1]} <= ${packageFilePath.replace(process.cwd(), '')}`,
+      };
+    } else {
+      console.log(`${depName} not in package-lock`);
+    }
+  } catch (err) {
+    console.log(chalk.red(err));
+  }
+}
+
+function checkDep(dep, ENV) {
+  dep.children = dep.children || [];
+  const pathToDep = `${process.cwd()}/node_modules/${dep.name}`;
+  const appPackageFilePath = `${process.cwd()}/package.json`;
+  const depPackageFilePath = `${pathToDep}/package.json`;
+  if (isSymlink(pathToDep)) {
+    ENV.symlinks = ENV.symlinks || [];
+    const currentSymlink = {
+      symlinkedDep: `${dep.name} (${checkGitState(pathToDep)})`,
+      symlinkedChildren: [],
+    };
+    dep.children.forEach((child) => {
+      const pathToChild = `${pathToDep}/node_modules/${child}`;
+      if (isSymlink(pathToChild)) {
+        currentSymlink.symlinkedChildren.push(`${child} (${checkGitState(pathToChild)})`);
+      }
+    });
+    ENV.symlinks.push(currentSymlink);
+  }
+  ENV.dependencySummary[dep.name] = {};
+  ENV.dependencySummary[dep.name] = installedMatchesRequired(appPackageFilePath, dep.name);
+  dep.children.forEach((child) => {
+    ENV.dependencySummary[child] = {};
+    ENV.dependencySummary[child] = installedMatchesRequired(depPackageFilePath, child);
+  });
+}
--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+const chalk = require('chalk');
+const path = require('path');
+const fs = require('fs');
+const simpleGit = require('simple-git');
+
 module.exports = {
   gitState(options) {
     const gitState = require('git-state');
@@ -16,5 +21,128 @@ module.exports = {
       return output;
     }
     return false;
+  },
+
+  commitPackage: async function (packageGit, packageConfig, logColour) {
+    await packageGit.add('.');
+    console.log(chalk.cyan(`[${packageConfig.name}] Added untracked files`));
+    let packageCommitMessage = packageConfig.commitMessage;
+    const commitOptions = {};
+    if (packageConfig.amendLatestCommit) {
+      commitOptions['--amend'] = true;
+    }
+    if (packageConfig.amendLatestCommit === 'no-edit') {
+      commitOptions['--no-edit'] = true;
+      packageCommitMessage = [];
+    }
+    const packageCommitResult = await packageGit.commit(packageCommitMessage, commitOptions);
+    const newSha = packageCommitResult.commit.length ? packageCommitResult.commit : null;
+    if (packageConfig.amendLatestCommit === true) {
+      console.log(chalk[logColour](`[${packageConfig.name}] Amend latest commit with an updated commit message ${await this.latestCommit(packageGit)}`));
+    } else if (newSha) {
+      if (packageConfig.amendLatestCommit === 'no-edit') {
+        console.log(chalk[logColour](`[${packageConfig.name}] Amend latest commit the same commit message to ${newSha} in branch ${packageCommitResult.branch}: ${JSON.stringify(packageCommitResult.summary)}`));
+      } else {
+        console.log(chalk[logColour](`[${packageConfig.name}] Add commit ${newSha} in branch ${packageCommitResult.branch}: ${JSON.stringify(packageCommitResult.summary)}`));
+      }
+    } else {
+      console.log(chalk[logColour](`[${packageConfig.name}] Nothing to commit - head is still at ${await this.latestCommit(packageGit)}`));
+    }
+  },
+
+  logResults: async function (results, localConfig) {
+    console.log('RESULT');
+    console.log(results);
+    console.log('SKIPPED');
+    const dependentPackagesSkippedGit = [];
+    const dependentPackagesSkipped = localConfig.localDependents.filter((item) => item.skip);
+    for (const item of dependentPackagesSkipped) {
+      const repoPath = path.resolve(process.cwd(), item.localRepoPath);
+      item.name = path.basename(item.localRepoPath);
+      item.git = simpleGit({
+        baseDir: repoPath,
+      });
+      dependentPackagesSkippedGit.push({
+        app: path.basename(item.localRepoPath),
+        gitState: this.gitState(item),
+      });
+    }
+    console.log(dependentPackagesSkippedGit);
+  },
+
+  updatePackageVersion(dependentPackage, version, packageConfig) {
+    const packageFilePath = path.resolve(packageConfig.localRepoPath, 'package.json');
+    const packageFile = require(packageFilePath);
+    if (packageFile.dependencies[dependentPackage.packageName].split('#')[1] === version) {
+      console.log(chalk.cyan(`[${packageConfig.name}] Version already set to ${version}, no update required.`));
+      return;
+    }
+    const dependentPackagePackageLink = packageFile.dependencies[dependentPackage.packageName].split('#')[0];
+    packageFile.dependencies[dependentPackage.packageName] = `${dependentPackagePackageLink}#${version}`;
+    fs.writeFileSync(packageFilePath, JSON.stringify(packageFile, null, 2));
+    console.log(chalk.cyan(`[${packageConfig.name}] Updated version of ${dependentPackagePackageLink} to ${version}`));
+  },
+
+  pushPackage: async function (packageGit, packageConfig, logColour) {
+    const pushOptions = [];
+    if (packageConfig.amendLatestCommit) {
+      pushOptions.push('-f');
+    }
+    const parentPackagePush = await packageGit.push(pushOptions);
+    const pushMessage = pushOptions.indexOf('-f') > -1 ? 'Force pushed code' : 'Pushed code';
+    const parentPackagePushMessage = (parentPackagePush.pushed[0] || {}).alreadyUpdated ? 'Already pushed' : pushMessage;
+    console.log(chalk[logColour](`[${packageConfig.name}] ${parentPackagePushMessage}`));
+  },
+
+  initialiseRepo: async function (repoName, git, logColour, branchLockItem, localConfig) {
+    const branch = await this.branchLockPass(repoName, git, logColour, branchLockItem, localConfig);
+    if (!branch) {
+      throw 'Error';
+    }
+    await git.fetch('origin', branch);
+    const remoteCommits = (await git.raw('rev-list', `origin/${branch}`)).split('\n');
+    const localCommits = (await git.raw('rev-list', `${branch}`)).split('\n');
+    if (localCommits.indexOf(remoteCommits[0]) < 0 && remoteCommits.indexOf(localCommits[0]) < 0) {
+      // Remote and local have diverged
+      throw `[${repoName}] ${branch} and origin/${branch} have diverged. This must be resolved before continuing.`;
+    } else if (localCommits[0] === remoteCommits[0]) {
+      // Local is up top date with remote
+      console.log(chalk[logColour](`[${repoName}] ${branch} is up to date with origin/${branch}.`));
+    } else if (localCommits.indexOf(remoteCommits[0]) > -1 && remoteCommits.indexOf(localCommits[0]) < 0) {
+      // Local ahead of remote
+      console.log(chalk[logColour](`[${repoName}] ${branch} is ahead of origin/${branch} and can be pushed.`));
+    } else if (remoteCommits.indexOf(localCommits[0]) > -1 && localCommits.indexOf(remoteCommits[0]) < 0) {
+      // Remote ahead of local
+      console.log(chalk[logColour](`[${repoName}] origin/${branch} is ahead of ${branch}.`));
+      if ((await git.status()).isClean()) {
+        await git.pull();
+        console.log(chalk[logColour](`[${repoName}] Pulled ${branch} branch.`));
+      } else {
+        throw `[${repoName}] origin/${branch} is ahead of ${branch} but ${branch} has uncommitted changes. This must be resolved before continuing.`;
+      }
+    }
+    console.log(chalk[logColour](`[${repoName}] ${branch} - initialisation complete.`));
+  },
+
+  branchLockPass: async function (appName, git, logColour, branchLockItem, localConfig) {
+    try {
+      if (!branchLockItem[appName]) {
+        throw `[${appName}] Error - the branch lock entry which includes ${localConfig.parentPackage.name}: ${branchLockItem[localConfig.parentPackage.name]}" does not specify a branch for ${appName}.`;
+      }
+      const currentAppBranch = (await git.branch()).current;
+      if (branchLockItem[appName] !== currentAppBranch) {
+        console.log(chalk[logColour](`[${appName}] Switching from branch "${currentAppBranch}" to "${branchLockItem[appName]}" as per branch lock entry.`));
+        await git.checkout(branchLockItem[appName]);
+      }
+      return (await git.branch()).current;
+    } catch (err) {
+      console.log(chalk.red(err));
+      throw err;
+    }
+  },
+
+  latestCommit: async function (git) {
+    const gitLog = await git.log();
+    return ((gitLog.all || [])[0] || {}).hash;
   },
 };

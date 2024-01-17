7087b38ec3ff9dc36a72658a254354e750f8480b
Andrew Paterson <andrew.paterson9@gmail.com>
2024-01-17 11:28:45 +0200

    More PNPM updates

--- dependency-checks.js
+++ dependency-checks.js
@@ -5,28 +5,30 @@ const path = require('path');
 const chalk = require('chalk');
 const nodeSundries = require('node-sundries');
 const util = require('util');
+const { type } = require('os');
 
 module.exports = function (environment, ENV, deps, localDevRepos) {
   try {
-    console.log(
-      `----------------------------------\n${chalk.magenta(
-        `MAP OF LINKED DEPENDENCIES`
-      )}\n`
-    );
-    console.log(
-      util.inspect(
-        filterProperties(getLocalLinks(process.cwd()), [
-          'name',
-          'absolutePath',
-          'children',
-        ]),
-        {
+    const linkedDeps = filterProperties(getLocalLinks(process.cwd()), [
+      'name',
+      'absolutePath',
+      'children',
+    ]);
+    if (linkedDeps.length) {
+      console.log(
+        `----------------------------------\n${chalk.magenta(
+          'MAP OF LINKED DEPENDENCIES'
+        )}\n`
+      );
+      console.log(
+        util.inspect(linkedDeps, {
           showHidden: false,
           depth: null,
           colors: true,
-        }
-      )
-    );
+        })
+      );
+    }
+
     if (environment === 'development' || environment === 'test') {
       ENV.dependencySummary = {};
       deps.forEach((dep) => {
@@ -34,11 +36,17 @@ module.exports = function (environment, ENV, deps, localDevRepos) {
       });
       console.log(
         `----------------------------------\n${chalk.magenta(
-          `DEPENDENCY VERSIONS`
+          'DEPENDENCY VERSIONS'
         )}\n`
       );
-      console.log(ENV.dependencySummary);
-      console.log(`----------------------------------`);
+      console.log(
+        util.inspect(ENV.dependencySummary, {
+          showHidden: false,
+          depth: null,
+          colors: true,
+        })
+      );
+      console.log('----------------------------------');
 
       checkLocalDevRepos(localDevRepos);
     }
@@ -49,6 +57,8 @@ module.exports = function (environment, ENV, deps, localDevRepos) {
 
 function allPnpmDeps(filePath, depTypes = ['dependencies', 'devDependencies']) {
   const json = nodeSundries.yamlFileToJs(`${filePath}/pnpm-lock.yaml`);
+  fs.writeFileSync('./trash.json', JSON.stringify(json, null, 2));
+
   const packageFile = require(path.resolve(
     process.cwd(),
     filePath,
@@ -79,19 +89,29 @@ function allPnpmDeps(filePath, depTypes = ['dependencies', 'devDependencies']) {
   }, []);
 }
 
-function installedMatchesRequired(item) {
+function specifiedMatchesInstalledDep(item) {
   if (item.linked) {
     return `Linked locally to ${item.absolutePath} ${item.gitState}`;
   }
-  const pnpmLockVersion =
-    item.pnpmLock.specifier.split('#')[1] || item.pnpmLock.specifier;
-  const packageJsonVersion = item.packageJson.split('#')[1] || item.packageJson;
-  if (pnpmLockVersion === packageJsonVersion) {
-    return pnpmLockVersion;
+  const specified = extractVersion(item.pnpmLock.specifier);
+  const installed = extractVersion(item.pnpmLock.version);
+  return expectedVsFoundOutput(specified, installed);
+}
+
+function expectedVsFoundOutput(specified, installed) {
+  if (
+    extractSemverNumbers(specified) &&
+    extractSemverNumbers(specified) === extractSemverNumbers(installed)
+  ) {
+    return extractSemverNumbers(installed);
   }
+  if (specified === installed) {
+    return installed;
+  }
+
   return {
-    pnpmLockVersion: pnpmLockVersion,
-    packageJsonVersion: packageJsonVersion,
+    specified: specified,
+    installed: extractSemverNumbers(installed) || installed,
   };
 }
 
@@ -99,10 +119,40 @@ function findPnpmDep(depName, filePath) {
   return allPnpmDeps(filePath).find((pnpmDep) => pnpmDep.name === depName);
 }
 
-function findPnpmDeps(depNames, filePath) {
-  return depNames
-    .map((depName) => findPnpmDep(depName, filePath))
-    .filter((item) => item);
+function extractHash(string) {
+  const hashRegex = /[0-9a-f]{40}/;
+  if (!string.match(hashRegex)) {
+    return false;
+  }
+  return string.match(hashRegex)[0];
+}
+
+function extractSemverString(string) {
+  const versionRegex = /.{0,1}[0-9]+\.[0-9]+\.[0-9]+/;
+  if (!string.match(versionRegex)) {
+    return false;
+  }
+  return string.match(versionRegex)[0];
+}
+
+function extractSemverNumbers(string) {
+  const versionRegex = /.{0,1}([0-9]+\.[0-9]+\.[0-9]+)/;
+  if (!string.match(versionRegex)) {
+    return false;
+  }
+  return string.match(versionRegex)[1];
+}
+
+function extractVersion(string) {
+  if (extractHash(string)) {
+    return extractHash(string);
+  }
+  if (extractSemverString(string)) {
+    return extractSemverString(string);
+  }
+  console.log(string);
+
+  return false;
 }
 
 function getLocalLinks(linkPath, acc = [], parent) {
@@ -122,27 +172,78 @@ function getLocalLinks(linkPath, acc = [], parent) {
 
 function checkDep(dep, ENV) {
   const pnpmDep = findPnpmDep(dep.name, './');
-  let childDeps = [];
+  const final = {
+    version: specifiedMatchesInstalledDep(pnpmDep),
+  };
   if (dep.children) {
-    if (pnpmDep.linked) {
-      childDeps = findPnpmDeps(dep.children, pnpmDep.absolutePath).map(
-        (item) => {
-          item.via = pnpmDep.absolutePath;
-          return item;
-        }
-      );
-    } else {
-      childDeps = findPnpmDeps(dep.children, './');
+    final.children = processChildPackages(pnpmDep, dep.children);
+  }
+  ENV.dependencySummary[pnpmDep.name] = final;
+}
+
+function processChildPackages(parent, children) {
+  const parentName = typeof parent === 'string' ? parent : parent.name;
+  const final = {};
+  if ((parent || {}).linked) {
+    children.forEach((child) => {
+      const keyName = typeof child === 'string' ? child : child.name;
+      final[keyName] = processChildOfLinkedPackage(parent, child);
+    });
+  } else {
+    children.forEach((child) => {
+      const keyName = typeof child === 'string' ? child : child.name;
+      final[keyName] = processChildPackage(parentName, child, './');
+    });
+  }
+  return final;
+}
+
+function processChildOfLinkedPackage(pnpmDep, child) {
+  const childDepName = typeof child === 'string' ? child : child.name;
+  const childPnpmDep = findPnpmDep(childDepName, pnpmDep.absolutePath);
+  childPnpmDep.via = pnpmDep.absolutePath;
+  return typeof child === 'string'
+    ? specifiedMatchesInstalledDep(childPnpmDep)
+    : {
+        version: specifiedMatchesInstalledDep(childPnpmDep),
+        children: processChildPackages(childPnpmDep, child.children),
+      };
+}
+
+function processChildPackage(parentDep, childPackage, filePath) {
+  const childDepName =
+    typeof childPackage === 'string' ? childPackage : childPackage.name;
+  const json = nodeSundries.yamlFileToJs(`${filePath}/pnpm-lock.yaml`);
+  let parentPackage;
+  const final = {};
+  for (const key in json.packages) {
+    if (key.indexOf(parentDep) > -1) {
+      parentPackage = json.packages[key];
     }
   }
-  const final = {
-    version: installedMatchesRequired(pnpmDep),
-  };
-  childDeps.forEach((childDep) => {
-    final.children = final.children || {};
-    final.children[childDep.name] = installedMatchesRequired(childDep);
-  });
-  ENV.dependencySummary[pnpmDep.name] = final;
+  for (const key in parentPackage.dependencies) {
+    if (childDepName === key) {
+      final.expected = extractVersion(parentPackage.dependencies[key]);
+    }
+  }
+  let package;
+  for (const key in json.packages) {
+    // Check if any of the items in the childPackages array are in the array produced by splitting the key on '/' and '@' in the key
+    const keyArray = key.split(/\/|@/);
+    if (keyArray.indexOf(childDepName) > -1) {
+      package = json.packages[key];
+      final.found =
+        package.resolution.commit || package.resolution.tarball
+          ? extractHash(package.resolution.commit || package.resolution.tarball)
+          : extractSemverString(key);
+    }
+  }
+  return typeof childPackage === 'string'
+    ? expectedVsFoundOutput(final.expected, final.found)
+    : {
+        version: expectedVsFoundOutput(final.expected, final.found),
+        children: processChildPackages(childDepName, childPackage.children),
+      };
 }
 
 function filterProperties(array, properties) {
